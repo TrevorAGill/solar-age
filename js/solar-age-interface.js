3cde2af3aff1e12f6324034f967bf9de672d42d0
Trevor Gill <trevor.a.gill@gmail.com>
2017-09-08 11:53:13 -0700

    set up gulp, bower, and karma, and include barebones js and html files

---
+++ js/solar-age-interface.js
@@ -0,0 +1,30 @@
+import {Age} from './../js/solar-age.js'
+
+$(function(){
+  $("#solarAge").submit(function(event){
+    event.preventDefault();
+
+    let birthDate = $("input#birthDate").val();
+    let currentDate = new Date();
+    $("#birthDate").text(birthDate);
+    $("#currentDate").text(currentDate);
+
+
+
+    // var line1 = $("input#haikuLine1").val();
+    // var line2 = $("input#haikuLine2").val();
+    // var line3 = $("input#haikuLine3").val();
+    // var newHaiku = new Haiku(line1,line2,line3);
+    // var wordArray1 = newHaiku.translateLineToArray(line1);
+    // var wordArray2 = newHaiku.translateLineToArray(line2);
+    // var wordArray3 = newHaiku.translateLineToArray(line3);
+    // var sylCountLine1 = newHaiku.loopThroughAllWords(wordArray1);
+    // var sylCountLine2 = newHaiku.loopThroughAllWords(wordArray2);
+    // var sylCountLine3 = newHaiku.loopThroughAllWords(wordArray3);
+    // $("#firstLineSyllables").text(sylCountLine1);
+    // $("#secondLineSyllables").text(sylCountLine2);
+    // $("#thirdLineSyllables").text(sylCountLine3);
+    // var output = newHaiku.isValidHaiku(sylCountLine1, sylCountLine2, sylCountLine3);
+    // $("#output").text(output);
+  });
+});
---
+++ js/solar-age.js
@@ -0,0 +1,83 @@
+export class Age {
+
+  constructor(birthDate, currentDate) {
+    this.birthDate = birthDate;
+    this.currentDate = currentDate;
+  }
+
+
+
+
+
+
+
+
+
+
+
+  translateLineToArray(lineInput){
+    let lineArray = lineInput.split(" ");
+    return lineArray;
+  }
+
+  loopThroughAllWords(lineArray){
+    let lineSyllableCount = 0;
+    for(let i=0 ; i<lineArray.length ; i++){
+      let letterArray = this.splitEachWordIntoArray(lineArray[i]);
+      let wordVowelCount = this.countVowels(letterArray);
+      let silentVowelCount = this.isSilentTwoVowelsTogether(letterArray) + this.isSilentEndsInE(letterArray);
+      let wordSyllableCount = wordVowelCount - silentVowelCount;
+      lineSyllableCount += wordSyllableCount;
+    }
+    return lineSyllableCount;
+  }
+
+  splitEachWordIntoArray(word){
+    let letterArray = word.split("");
+    return letterArray;
+  }
+
+  countVowels(letterArray){
+    let vowelCount = 0;
+    for(let i=0 ; i<letterArray.length ; i++){
+      if(this.vowels.includes(letterArray[i])){
+        vowelCount++;
+      }
+    }
+    return vowelCount;
+  }
+
+  isSilentTwoVowelsTogether(letterArray){
+    let silentVowelCount = 0;
+    for(let i=0 ; i<letterArray.length ; i++){
+      if(this.vowels.includes(letterArray[i]) && this.vowels.includes(letterArray[i+1])){
+        silentVowelCount++;
+      }
+    }
+    return silentVowelCount;
+  }
+
+  isSilentEndsInE(letterArray){
+    let silentVowelCount = 0;
+    let lastLetterIndex = letterArray.length;
+    let lastLetter = letterArray[lastLetterIndex-1];
+    if((lastLetter === "e"  && letterArray[lastLetterIndex - 2] != "l") || (lastLetter === "s"  && letterArray[lastLetterIndex - 2] === "e" && letterArray[lastLetterIndex - 3] !="l")){
+      silentVowelCount = 1;
+    }
+    return silentVowelCount;
+  }
+
+  isValidHaiku(sylCountLine1, sylCountLine2, sylCountLine3){
+    if(sylCountLine1 != 5 || sylCountLine2 != 7 || sylCountLine3 != 5){
+      return "Not a Haiku!";
+    }
+    return "You made a Haiku!";
+  }
+
+
+  //     }
+  //   }
+  //   return ruleMetCount;
+  // }
+
+}
