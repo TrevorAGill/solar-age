2af131809108de2ae0ac3a3192fd409f88b63957
Trevor Gill <trevor.a.gill@gmail.com>
2017-09-08 16:07:30 -0700

    include significant progress on integrating life expectancy API

--- js/solar-age.js
+++ js/solar-age.js
@@ -6,9 +6,9 @@ export class Age {
     this.remainingLife = 0;
   }
 
-  calculateAgeInSeconds(birthDate,currentDate){
-    let currentDateTime = currentDate.getTime();
-    let birthDateTime = birthDate.getTime();
+  calculateAgeInSeconds(){
+    let currentDateTime = this.currentDate.getTime();
+    let birthDateTime = this.birthDate.getTime();
     let differenceBetweenDates = currentDateTime - birthDateTime;
     let secondsAliveOnEarth = Math.floor(differenceBetweenDates / 1000);
     return secondsAliveOnEarth;
@@ -16,14 +16,14 @@ export class Age {
 
   calculateAgeInMercuryYears(secondsAliveOnEarth){
     const secondsInEarthYear = 31536000;
-    let ageRaw = secondsAliveOnEarth / (secondsInEarthYear * .24);
+    let ageRaw = secondsAliveOnEarth / (secondsInEarthYear * 0.24);
     let age = parseFloat(ageRaw.toFixed(2));
     return age;
   }
 
   calculateAgeInVenusYears(secondsAliveOnEarth){
     const secondsInEarthYear = 31536000;
-    let ageRaw = secondsAliveOnEarth / (secondsInEarthYear * .62);
+    let ageRaw = secondsAliveOnEarth / (secondsInEarthYear * 0.62);
     let age = parseFloat(ageRaw.toFixed(2));
     return age;
   }
@@ -40,6 +40,27 @@ export class Age {
     let ageRaw = secondsAliveOnEarth / (secondsInEarthYear * 11.86);
     let age = parseFloat(ageRaw.toFixed(2));
     return age;
+  }
+
+  convertAgeToAPIFormat(){
+    let currentAgeInSeconds = this.calculateAgeInSeconds();
+    let exactAgeInYears= currentAgeInSeconds/31536000;
+    let yearsAlive = Math.floor(exactAgeInYears);
+    let monthsAlive = Math.floor((exactAgeInYears % 1) * 12);
+    let finalOutput = yearsAlive + "y" + monthsAlive + "m";
+    return finalOutput;
+  }
+
+  convertCurrentDateToAPIFormat(){
+    let finalOutput = this.currentDate.toISOString().slice(0, 10);
+    return finalOutput;
+  }
+
+  craftApiUrl(sex,country){
+    let currentDateAPIRequiredFormat = this.convertCurrentDateToAPIFormat();
+    let ageAPIRequiredFormat = this.convertAgeToAPIFormat();
+    let url = "http://api.population.io:80/1.0/life-expectancy/remaining/" + sex + "/" + country + "/" + currentDateAPIRequiredFormat + "/" + ageAPIRequiredFormat + "/"
+    return url;
   }
 
 
